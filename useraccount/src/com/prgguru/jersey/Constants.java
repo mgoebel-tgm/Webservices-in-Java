80970d73a8c222a91835f8aea4d3c9b381bcbe54
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2016-02-19 08:20:04 +0100

    Init commit

---
+++ useraccount/src/com/prgguru/jersey/Constants.java
@@ -0,0 +1,9 @@
+package com.prgguru.jersey;
+//Change these parameters according to your DB
+public class Constants {
+    public static String dbClass = "com.mysql.jdbc.Driver";
+    private static String dbName= "users";
+    public static String dbUrl = "jdbc:mysql://10.0.105.61:3306/"+dbName;
+    public static String dbUser = "schinken";
+    public static String dbPwd = "password";
+}
